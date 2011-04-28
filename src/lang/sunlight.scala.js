10e5388dea7099c03816f511f0cc24380224ded5
Tommy Montgomery <tommy.mont@gmail.com>
2011-04-28 05:48:40 +0000

    a little bit more of the scala implementation

--- src/lang/sunlight.scala.js
+++ src/lang/sunlight.scala.js
@@ -71,19 +71,64 @@
 		},
 		
 		identFirstLetter: /[A-Za-z]/,
-		identAfterFirstLetter: /[\w-]/,
+		identAfterFirstLetter: /\w/,
+		
+		customParseRules: [
+			//case classes: can't distinguish between a case class and a function call so we need to keep track of them
+			function(context) {
+				if (context.defaultData.text === "") {
+					return false;
+				}
+				
+				if (!/[A-Za-z]/.test(context.reader.current())) {
+					return false;
+				}
+				
+				var matches = sunlight.util.createProceduralRule(context.count() - 1, -1, [{ token: "keyword", values: ["case"] }, { token: "default" }, { token: "keyword", values: ["class"] }])(context.getAllTokens());
+				if (!matches) {
+					return false;
+				}
+				
+				//this is a case class definition, read the class name
+				var peek, className = context.reader.current(), line = context.reader.getLine(), column = context.reader.getColumn();
+				while (peek = context.reader.peek()) {
+					if (!/\w/.test(peek)) {
+						break;
+					}
+					
+					className += context.reader.read();
+				}
+				
+				context.items.caseClasses.push(className);
+				
+				return context.createToken("ident", className, line, column);
+			}
+		],
 
 		namedIdentRules: {
+			custom: [	
+				//user-defined case classes
+				function(context) {
+					return sunlight.util.contains(context.items.caseClasses, context.tokens[context.index].value);
+				}
+				
+			],
+			
 			follows: [
-				[{ token: "keyword", values: ["class", "object", "extends", "new"] }, { token: "default" }],
+				[{ token: "keyword", values: ["class", "object", "extends", "new", "type", "trait"] }, { token: "default" }],
 				[{ token: "operator", values: [":"] }, sunlight.util.whitespace],
 				[{ token: "operator", values: ["#"] }]
+			],
+			
+			precedes: [
+				//[{ token: "punctuation", values: ["["] }, sunlight.util.whitespace]
 			]
 		},
 		
 		contextItems: {
 			literalXmlOpenTag: null,
-			literalXmlNestingLevel: 0
+			literalXmlNestingLevel: 0,
+			caseClasses: []
 		},
 
 		operators: [
