ff3fe1d07aa4d70cb9a0da92add33d90bb2f6939
tmont <tommy.mont@gmail.com>
2013-09-03 19:19:17 -0700

    more tests for 6502asm

--- tests/lang/6502asm-tests.js
+++ tests/lang/6502asm-tests.js
@@ -6,20 +6,54 @@ describe('6502 ASM', function() {
 	highlighter.register('6502asm', require('../../src/lang/6502asm'));
 	var nbsp = String.fromCharCode(0xA0);
 
-	it('should highlight labels', function() {
+	it('label', function() {
 		var highlighted = highlighter.highlight('FooBar', '6502asm').result;
 		highlighted.should.equal('<span class="sunlight-6502asm"><span class="sunlight-label">FooBar</span></span>');
 	});
 
-	it('should highlight keywords', function() {
-		var highlighted = highlighter.highlight('FooBar\n  sta', '6502asm').result,
-			expected = '<span class="sunlight-6502asm"><span class="sunlight-label">FooBar</span>\n' + nbsp + nbsp + '<span class="sunlight-keyword">sta</span></span>';
+	it('label even if it is a keyword if it starts a line', function() {
+		var highlighted = highlighter.highlight('sta', '6502asm').result,
+			expected = '<span class="sunlight-6502asm"><span class="sunlight-label">sta</span></span>';
 		highlighted.should.equal(expected);
 	});
 
-	it('should highlight comments', function() {
+	it('keywords', function() {
+		var highlighted = highlighter.highlight('FooBar\n sta', '6502asm').result,
+			expected = '<span class="sunlight-6502asm"><span class="sunlight-label">FooBar</span>\n' + nbsp + '<span class="sunlight-keyword">sta</span></span>';
+		highlighted.should.equal(expected);
+	});
+
+	it('comments', function() {
 		var highlighted = highlighter.highlight('; lolz', '6502asm').result,
 			expected = '<span class="sunlight-6502asm"><span class="sunlight-comment">;' + nbsp + 'lolz</span></span>';
+		highlighted.should.equal(expected);
+	});
+
+	it('single-quoted strings', function() {
+		var highlighted = highlighter.highlight('\'foo\'', '6502asm').result,
+			expected = '<span class="sunlight-6502asm"><span class="sunlight-string">\'foo\'</span></span>';
+		highlighted.should.equal(expected);
+	});
+
+	it('pseudo op', function() {
+		var highlighted = highlighter.highlight('Foo\n .byte', '6502asm').result,
+			expected = '<span class="sunlight-6502asm"><span class="sunlight-label">Foo</span>\n' +
+				nbsp + '<span class="sunlight-punctuation">.</span>' +
+				'<span class="sunlight-pseudoOp">byte</span></span>';
+		highlighted.should.equal(expected);
+	});
+
+	it('constant', function() {
+		var highlighted = highlighter.highlight('Foo\n ldy #PLAYERAREAHEIGHT', '6502asm').result,
+			expected = '<span class="sunlight-6502asm"><span class="sunlight-label">Foo</span>\n' +
+				nbsp + '<span class="sunlight-keyword">ldy</span>' + nbsp +
+				'<span class="sunlight-constant">#PLAYERAREAHEIGHT</span></span>';
+		highlighted.should.equal(expected);
+	});
+
+	it('number', function() {
+		var highlighted = highlighter.highlight('$2C', '6502asm').result,
+			expected = '<span class="sunlight-6502asm"><span class="sunlight-number">$2C</span></span>';
 		highlighted.should.equal(expected);
 	});
 });
